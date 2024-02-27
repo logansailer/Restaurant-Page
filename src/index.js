86635eabe9cbdae2b203215629b59fa90a3ff31a
Logan Sailer <logansailer0633@gmail.com>
2024-02-27 16:12:09 +0900

    fixed text sizing and margins

--- src/index.js
+++ src/index.js
@@ -13,14 +13,18 @@ function home() {
     contentDiv.appendChild(myPromo);
 
     //adds image caption under
+    const textField = document.createElement('div')
+    textField.classList.add('textDiv')
     const advert = document.createElement("h2");
     const adCaption = document.createTextNode("Try New Savory Chili WcDonald's Sauce");
-    const advertText = document.createElement("p");
-    const text = document.createTextNode("Discover the new Savory Chili WcDonald’s Sauce,* straight from the WcDonald’s universe. So savory and bold! It’s the special move you’ll shout Shounen-style when you activate and unlock 2x bonus points on your next 10 piece Chicken McNuggets® order in the app.^ Plus, enjoy four weekly, unique anime and manga exclusives.")
-    advertText.appendChild(text);
+    const advertTextElement = document.createElement("p");
+    const adText = document.createTextNode("Discover the new Savory Chili WcDonald’s Sauce,* straight from the WcDonald’s universe. So savory and bold! It’s the special move you’ll shout Shounen-style when you activate and unlock 2x bonus points on your next 10 piece Chicken McNuggets® order in the app.^ Plus, enjoy four weekly, unique anime and manga exclusives.")
+    advertTextElement.appendChild(adText);
     advert.appendChild(adCaption);
-    contentDiv.appendChild(advert);
-    contentDiv.appendChild(advertText);
+    textField.appendChild(advert);
+    textField.appendChild(advertTextElement);
+    contentDiv.appendChild(textField)
+
     
 }
 
