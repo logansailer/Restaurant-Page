ac2043747b4908c5f1b0bef6f4f724fca65ee3e5
Logan Sailer <logansailer0633@gmail.com>
2024-02-28 09:40:34 +0900

    changed home function to accept multiple adds

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-//load home on site open
-//each button deletes home and loads another page
 import './styles.css';
-import promo from './wcdonalds.jpg';
+import wcDonaldsPromo from './wcdonalds.jpg';
+import hamburglerPromo from './hamburgler.jpg'
 import logo from './logo.jpg';
 
+//header and page selection document queries
 const logoDiv = document.querySelector('#logo')
 const contentDiv = document.querySelector('#content')
 const mcLogo = new Image()
 mcLogo.src = logo;
+logoDiv.appendChild(mcLogo);
 
-function home() {
+//text for add campaigns
+const wcDonaldsHeading = "Try New Savory Chili WcDonald's Sauce";
+const wcDonaldsText = "Discover the new Savory Chili WcDonald's Sauce,* straight from the WcDonald's universe. So savory and bold! It's the special move you'll shout Shounen-style when you activate and unlock 2x bonus points on your next 10 piece Chicken McNuggets® order in the app.^ Plus, enjoy four weekly, unique anime and manga exclusives.";
+const hamburglerHeading = `McD's Best Burgers Ever.” - Hamburglar`;
+const hamburglerText = `You're gonna love the hotter, juicier, tastier upgrades we've made to burgers like our Cheeseburger and Double Cheeseburger.* From patties grilled with onions and perfectly melted cheese to new soft, pillowy buns—these are our best burgers yet. Ask the Hamburglar, he can't keep his hands off of 'em. Robble, robble.`;
+
+function loadHome(adImg, adHeading, adText) {
     //adds promo image to home page
     const myPromo = new Image()
-    myPromo.src = promo;
+    myPromo.src = adImg;
     myPromo.classList.add('promo');
     contentDiv.appendChild(myPromo);
 
@@ -20,15 +27,20 @@ function home() {
     const textField = document.createElement('div')
     textField.classList.add('textDiv')
     const advert = document.createElement("h2");
-    const adCaption = document.createTextNode("Try New Savory Chili WcDonald's Sauce");
+    const adCaption = document.createTextNode(adHeading);
     const advertTextElement = document.createElement("p");
-    const adText = document.createTextNode("Discover the new Savory Chili WcDonald's Sauce,* straight from the WcDonald's universe. So savory and bold! It's the special move you'll shout Shounen-style when you activate and unlock 2x bonus points on your next 10 piece Chicken McNuggets® order in the app.^ Plus, enjoy four weekly, unique anime and manga exclusives.")
-    advertTextElement.appendChild(adText);
+    const adSubText = document.createTextNode(adText)
+    advertTextElement.appendChild(adSubText);
     advert.appendChild(adCaption);
     textField.appendChild(advert);
     textField.appendChild(advertTextElement);
     contentDiv.appendChild(textField)
 }
 
-logoDiv.appendChild(mcLogo);
+//loads home page
+function home() {
+    loadHome(wcDonaldsPromo, wcDonaldsHeading, wcDonaldsText)
+    loadHome(hamburglerPromo, hamburglerHeading, hamburglerText)
+}
+
 home()
